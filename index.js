24062115673735c86ccfcbadc43e3a6131e27cff
stefanoruschettacaleffi <stefano.ruschetta@caleffi.com>
2017-10-27 15:59:43 +0200

    another commit

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express();
-app.get('/' , (req,res) => {
-	res.send("HEY OH! LET\'S GO! GO! go go go! Empra!")
-})
+
+app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
