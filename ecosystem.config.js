96ed3d6d82efdf750844c6be961c41935fe739f0
stefanoruschettacaleffi <stefano.ruschetta@caleffi.com>
2017-10-27 15:44:34 +0200

    server

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'basic',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-220-137-114.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/basicserverkeys.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:stefanoruschettacaleffi/nodeservercaleffi.git',
+      path: '/home/ubuntu/basic',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
 app.get('/' , (req,res) => {
-	res.send("HEY OH! LET\'S GO!")
+	res.send("HEY OH! LET\'S GO! GO!")
 })
 app.listen(3000, () => console.log('Server running on port 3000'))
