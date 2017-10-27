f907ef146f2dadca624c4edbe2635936d9c4b368
stefanoruschettacaleffi <stefano.ruschetta@caleffi.com>
2017-10-27 16:44:02 +0200

    another commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-18-220-137-114.us-east-2.compute.amazonaws.com',
+      host: 'ec2-18-221-205-23.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/basicserverkeys.pem',
       ref: 'origin/master',
       repo: 'git@github.com:stefanoruschettacaleffi/nodeservercaleffi.git',
