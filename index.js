3e0f4c4577dd4e5a05f60c3ec58a72b364e21116
stefanoruschettacaleffi <stefano.ruschetta@caleffi.com>
2017-10-30 10:54:02 +0100

    ip

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World')
-  console.log("here");
+
+  var ip = req.headers['X-Real-IP'] || req.connection.remoteAddress;
+
+  console.log("request ip: " + ip);
 })
+
 app.listen(3000, () => console.log('Server running on port 3000'))
