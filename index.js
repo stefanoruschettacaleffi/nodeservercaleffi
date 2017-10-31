4763d7495f641277ca39ce52c8a56464ce419e5c
stefanoruschettacaleffi <stefano.ruschetta@caleffi.com>
2017-10-31 14:55:26 +0100

    server

--- index.js
+++ index.js
@@ -27,7 +27,10 @@ function handleConnection(conn) {
   function onConnData(d) {
     console.log('connection data from %s: %j', remoteAddress, d);
     if(d == "e5"){
-      conn.write("+++", "utf-8");
+      conn.write("+", "utf-8");
+      setTimeout(sendPlus, 200);
+      setTimeout(sendPlus, 400);
+      setTimeout(sendPlus, 600);
     }
   }
 
@@ -37,5 +40,9 @@ function handleConnection(conn) {
 
   function onConnError(err) {
     console.log('Connection %s error: %s', remoteAddress, err.message);
+  }
+
+  function sendPlus(){
+    conn.write("+");
   }
 }
