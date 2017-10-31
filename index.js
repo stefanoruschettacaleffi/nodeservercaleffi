24e8f331b8ec7c68bc21c115825fc268716e3294
stefanoruschettacaleffi <stefano.ruschetta@caleffi.com>
2017-10-31 11:03:30 +0100

    server

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ function handleConnection(conn) {
   function onConnData(d) {
     console.log('connection data from %s: %j', remoteAddress, d);
     if(d == "e5"){
-      conn.write("107B017C16", "hex");
+      //conn.write("107B017C16", "hex");
+      conn.disconnect();
     }
   }
 
