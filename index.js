7f5406d6850b9e8cddfae58a2c8ae79bdc2bdd08
stefanoruschettacaleffi <stefano.ruschetta@caleffi.com>
2017-11-06 11:06:58 +0100

    server

--- index.js
+++ index.js
@@ -26,10 +26,10 @@ function handleConnection(conn) {
 
   function onConnData(d) {
     console.log('connection data from %s: %j', remoteAddress, d);
-    if(d == "e5"){
-      console.log("trying to disconnect.");
+    //if(d == "e5"){
+      //console.log("trying to disconnect.");
       conn.end();
-    }
+    //}
   }
 
   function onConnClose() {
